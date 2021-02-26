85c86460c396b01362a1cedd8068203f72341b2a
Daisy <daisy@vizir.com.br>
2021-02-25 21:27:04 -0300

    add styles

--- src/components/ListButtons.tsx
+++ src/components/ListButtons.tsx
@@ -3,10 +3,10 @@ import challenges from '../../schedule.json'
 
 export function ListButtons() {
   return (
-    <ul>
+    <div className={styles.listButtonsContainer}>
       {challenges.schedule.map((item, index) => {
-        return <a target="_blank" href={`https://wol.jw.org/pt/wol/l/r5/lp-t?q=${item}`} key={index}>{item}</a>
+        return <a className={styles.item} target="_blank" href={`https://wol.jw.org/pt/wol/l/r5/lp-t?q=${item}`} key={index}>{item}</a>
       })}
-    </ul>
+    </div>
   )
 }
